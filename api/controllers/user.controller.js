d21c8002926fdd51d19a7ac37a7775541cc0f4ce
Nmistry222 <Neilmistry.dl@gmail.com>
2024-05-24 22:18:01 +1000

    Complete Update user functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,40 +1,38 @@
 import bcryptjs from 'bcryptjs';
 import User from '../models/user.model.js';
+import {errorHandler} from '../utils/error.js'
 
-export const test =  (req, res) => { 
-    
-    res.json({message: "API Route is working!",
-
-    });
-
+export const test = (req, res) => {
+    res.json({ message: "API Route is working!" });
 }
 
-export const  updateUser = async (req, res, next ) => {
-
-    if(req.user.id !== req.params.id) return next(errorHandler(401, "Your are only to update your own account!" ))
-
-    try {
-        if(req.body.password) {
-            req.body.password = bcryptjs.hashSync(req.body.password, 10)
-
-            const updatedUser = await User.findByIdAndUpdate(req.params.id, {
-                $set: {
-                    username: req.body.username,
-                    email: req.body.email,
-                    password: req.body.password,
-                    avatar: req.body.avatar,
-
-                }
-            }, {new: true})
-
-            const {password, ...rest} = updatedUser._doc
-
-            res.status(200).json(rest)
-
-        }
-
-    } catch (error) {
-        next(error)
+export const updateUser = async (req, res, next) => {
+    // Check if the logged-in user is trying to update their own account
+    if (req.user.id !== req.params.id) {
+        return next(errorHandler(401, "You are only allowed to update your own account!"));
     }
 
+    try {
+        const updateFields = {};
+
+        // Check and update each field if it exists in the request body
+        if (req.body.username) updateFields.username = req.body.username;
+        if (req.body.email) updateFields.email = req.body.email;
+        if (req.body.avatar) updateFields.avatar = req.body.avatar;
+        if (req.body.password) {
+            updateFields.password = bcryptjs.hashSync(req.body.password, 10);
+        }
+
+        // Update the user document in the database
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+            $set: updateFields
+        }, { new: true });
+
+        // Remove the password field from the response
+        const { password, ...rest } = updatedUser._doc;
+
+        res.status(200).json(rest);
+    } catch (error) {
+        next(error);
+    }
 }
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -24,6 +24,18 @@ const userSlice = createSlice(
             signInFailure: (state,action) => {
                 state.error = action.payload;
                 state.loading = false;
+            },
+            updateUserStart: (state) => {
+                state.loading = true;
+            },
+            updateUserSuccess: (state,action) => {
+                state.currentUser = action.payload;
+                state.loading = false;
+                state.error = null;
+            },
+            updateUserFailure: (state,action) => {
+                state.error = action.payload
+                state.loading = false;
             }
 
         }
@@ -31,6 +43,6 @@ const userSlice = createSlice(
     }
 );
 
-export const {signInStart,  signInSuccess, signInFailure} = userSlice.actions;
+export const {signInStart,  signInSuccess, signInFailure, updateUserStart, updateUserSuccess, updateUserFailure} = userSlice.actions;
 
 export default userSlice.reducer;
