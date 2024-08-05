189523849956cd924e4bc4296cd3608291fcde97
Nmistry222 <Neilmistry.dl@gmail.com>
2024-08-05 23:39:29 +1000

    commit

--- api/index.js
+++ api/index.js
@@ -3,54 +3,49 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
-import listingRouter from './routes/listing.routing.js';
+import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
 import path from 'path';
-import { fileURLToPath } from 'url';
-
 dotenv.config();
 
-mongoose.connect(process.env.MONGO)
+mongoose
+  .connect(process.env.MONGO)
   .then(() => {
     console.log('Connected to MongoDB!');
   })
   .catch((err) => {
-    console.error(err);
+    console.log(err);
   });
 
-// For ES modules, __dirname is not available. Use fileURLToPath for equivalent.
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+  const __dirname = path.resolve();
 
 const app = express();
 
 app.use(express.json());
+
 app.use(cookieParser());
 
-// API Routes
-app.use("/api/user", userRouter);
-app.use("/api/auth", authRouter);
-app.use("/api/listing", listingRouter);
-
-// Serve static files from Vite build
-app.use(express.static(path.join(__dirname,  '/client/dist')));
-
-// Catch-all route for SPA
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!');
 });
 
-// Error Handling Middleware
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
+
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
+
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
-  res.status(statusCode).json({
+  return res.status(statusCode).json({
     success: false,
     statusCode,
     message,
   });
-});
-
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
 });
